4ac41d30e1b9d37a5971b0313d92d88a8009410a
Kevin Tran <kevint24@uw.edu>
2024-03-08 11:54:36 -0800

    refact: amplify app instead for dynamic rendering

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,37 +1,22 @@
 #!/usr/bin/env node
 import * as cdk from "aws-cdk-lib";
 import "source-map-support/register";
-import { DeploymentStack } from "../lib/DeploymentStack";
-import { FrontendStack } from "../lib/FrontendStack";
+import { AmplifyStack } from "../lib/AmplifyStack";
 
 const envVariables = {
 	environment: process.env["ENV"] ?? "",
 	projectName: process.env["PROJECT_NAME"] ?? "",
-	// Certificate manager only available for us-east-1 through CDK
-	region: "us-east-1",
 	accountId: process.env["AWS_ACCOUNT_ID"] ?? "",
 	domainName: process.env["DOMAIN_NAME"] ?? "",
-};
-
-const app = new cdk.App();
-
-const frontendStack = new FrontendStack(app, "FrontendStack", {
-	...envVariables,
-	env: {
-		region: envVariables.region,
-		account: envVariables.accountId,
-	},
-});
-
-new DeploymentStack(app, "DeploymentStack", {
-	...envVariables,
-	siteBucket: frontendStack.siteBucket,
+	region: process.env["AWS_REGION"] ?? "",
 	email: process.env["EMAIL"] ?? "",
 	owner: process.env["GITHUB_OWNER"] ?? "",
 	repo: process.env["GITHUB_REPO"] ?? "",
 	token: process.env["GITHUB_TOKEN"] ?? "",
-	env: {
-		region: envVariables.region,
-		account: envVariables.accountId,
-	},
+};
+
+const app = new cdk.App();
+
+new AmplifyStack(app, "AmplifyStack", {
+	...envVariables,
 });
---
+++ cdk/lib/AmplifyStack.ts
@@ -0,0 +1,112 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as fs from "fs";
+
+export interface AmplifyStackProps extends cdk.StackProps {
+	projectName: string;
+	environment: string;
+	accountId: string;
+	region: string;
+	domainName: string;
+	email: string;
+	owner: string;
+	repo: string;
+	token: string;
+}
+
+export class AmplifyStack extends cdk.Stack {
+	constructor(scope: Construct, id: string, props: AmplifyStackProps) {
+		super(scope, id, props);
+
+		const {
+			projectName,
+			environment,
+			accountId,
+			region,
+			domainName,
+			email,
+			owner,
+			repo,
+			token,
+		} = props;
+
+		const amplifyRole = new cdk.aws_iam.Role(this, "AmplifyRole", {
+			assumedBy: new cdk.aws_iam.ServicePrincipal("amplify.amazonaws.com"),
+		});
+
+		amplifyRole.addToPolicy(
+			cdk.aws_iam.PolicyStatement.fromJson({
+				Effect: "Allow",
+				Action: ["ssm:DescribeParameters"],
+				Resource: "*",
+			})
+		);
+		amplifyRole.addToPolicy(
+			cdk.aws_iam.PolicyStatement.fromJson({
+				Effect: "Allow",
+				Action: ["ssm:GetParameters"],
+				Resource: `arn:aws:ssm:${region}:${accountId}:parameter/${projectName}/${environment}/*`,
+			})
+		);
+
+		const amplifyApp = new cdk.aws_amplify.CfnApp(this, "AmplifyApp", {
+			name: projectName,
+			accessToken: token,
+			platform: "WEB_DYNAMIC",
+			enableBranchAutoDeletion: true,
+			repository: repo,
+			iamServiceRole: amplifyRole.roleArn,
+			environmentVariables: [
+				{
+					name: "PROJECT_NAME",
+					value: projectName,
+				},
+				{
+					name: "ENV",
+					value: environment,
+				},
+			],
+			buildSpec: fs.readFileSync("../cicd/buildspec.yml", "utf8"),
+		});
+
+		const appId = amplifyApp.attrAppId;
+
+		new cdk.aws_amplify.CfnBranch(this, "MasterBranch", {
+			appId,
+			branchName: "master",
+			description: `Master branch of ${projectName}`,
+		});
+
+		new cdk.aws_amplify.CfnBranch(this, "DevBranch", {
+			appId,
+			branchName: "dev",
+			description: `Dev branch of ${projectName}`,
+		});
+
+		new cdk.aws_amplify.CfnDomain(this, "Domain", {
+			appId,
+			domainName,
+			subDomainSettings: [
+				{
+					branchName: "master",
+					prefix: "",
+				},
+				{
+					branchName: "dev",
+					prefix: "dev",
+				},
+			],
+		});
+
+		// CloudFormation Outputs
+		new cdk.CfnOutput(this, "AmplifyAppARN", {
+			value: amplifyApp.attrArn,
+			description: "Amplify App ARN",
+		});
+
+		new cdk.CfnOutput(this, "AmplifyAppId", {
+			value: appId,
+			description: "Web storage bucket name",
+		});
+	}
+}
