4a248bb9de57fb227743297afb1157851571c0d7
Kevin Tran <kevint24@uw.edu>
2024-04-01 15:16:32 -0700

    refact: updated bug for keys

--- frontend/src/app/components/AboutSection.tsx
+++ frontend/src/app/components/AboutSection.tsx
@@ -94,7 +94,7 @@ const AboutSection = () => {
 													index !== technology.length - 1
 														? value + ", "
 														: value;
-												return <>{input}</>;
+												return <span key={index}>{input}</span>;
 											})}
 										</span>
 									</div>
